39d4933420ffb3adf1824bfbf44328012b3d5897
Carina Cunha <carina.cunha13@gmail.com>
2022-05-25 15:56:46 -0300

    Requisitos 1 e 2

---
+++ script.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-undef */
+function backgroundColor(color1, color2, color3, color4) {
+  const backgroundColor = document.getElementsByClassName('color');
+  backgroundColor[0].style.backgroundColor = color1;
+  backgroundColor[1].style.backgroundColor = color2;
+  backgroundColor[2].style.backgroundColor = color3;
+  backgroundColor[3].style.backgroundColor = color4;
+}
+
+color1 = 'rgb(111, 105, 172)';
+color2 = 'rgb(149, 218, 193)';
+color3 = 'rgb(255, 235, 161)';
+color4 = 'rgb(253, 111, 150)';
+
+// eslint-disable-next-line no-undef
+backgroundColor(color1, color2, color3, color4);
