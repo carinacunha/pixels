33c1796cb0046564452f04e6ac9d8a1460dc9ca2
Carina Cunha <carina.cunha13@gmail.com>
2022-05-25 23:25:21 -0300

    Refatorar código

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
 // Requisito 1, 2, e 3
-function backgroundColor(color1, color2, color3, color4) {
-  let backgroundColor = document.getElementsByClassName('color');
+function background(color1, color2, color3, color4) {
+  const backgroundColor = document.getElementsByClassName('color');
   backgroundColor[0].style.backgroundColor = color1;
   backgroundColor[1].style.backgroundColor = color2;
   backgroundColor[2].style.backgroundColor = color3;
   backgroundColor[3].style.backgroundColor = color4;
 }
 
-let color1 = 'black';
-let color2 = 'rgb(149, 218, 193)';
-let color3 = 'rgb(255, 235, 161)';
-let color4 = 'rgb(253, 111, 150)';
+const color1 = 'black';
+const color2 = 'rgb(149, 218, 193)';
+const color3 = 'rgb(255, 235, 161)';
+const color4 = 'rgb(253, 111, 150)';
 
-backgroundColor(color1, color2, color3, color4);
+background(color1, color2, color3, color4);
 
 // Requisio 4 e 5
-let divContainer = document.getElementById('pixel-board');
-let numberLines = 5;
-let numberColumns = 5;
+const divContainer = document.getElementById('pixel-board');
+const numberLines = 5;
+const numberColumns = 5;
 
 for (let i = 1; i <= numberLines; i += 1) {
-
-  let divLine = document.createElement('div');
+  const divLine = document.createElement('div');
   divLine.className = 'line';
   divContainer.appendChild(divLine);
 
   for (let j = 1; j <= numberColumns; j += 1) {
-    let divPixel = document.createElement('div');
+    const divPixel = document.createElement('div');
     divPixel.className = 'pixel';
     divLine.appendChild(divPixel);
   }
 }
 
 // Requisito 6
-
-let colorStart = document.getElementsByClassName('color')[0];
+const colorStart = document.getElementsByClassName('color')[0];
 colorStart.className += ' selected';
 
 // Requisito 7
 
-let palette = document.getElementById('color-palette');
+const palette = document.getElementById('color-palette');
 
-palette.addEventListener('click', function (event) {
-  let selectedClass = document.getElementsByClassName('color selected')[0];
+palette.addEventListener('click', (event) => {
+  const evt = event;
+  const selectedClass = document.getElementsByClassName('color selected')[0];
+
   selectedClass.className = 'color';
-  event.target.className += ' selected';
+  evt.target.className += ' selected';
 });
 
 // Requisto 8
+const pixels = document.getElementById('pixel-board');
 
-let pixels = document.getElementById('pixel-board');
+pixels.addEventListener('click', (event) => {
+  const evt = event;
+  const selectedColor = document.getElementsByClassName('color selected')[0];
 
-pixels.addEventListener('click', function (event) {
-  let selectedColor = document.getElementsByClassName('color selected')[0];
-  event.target.style.background = selectedColor.style.backgroundColor;
-})
+  evt.target.style.background = selectedColor.style.backgroundColor;
+});
 
 // Requisito 9
-
-let buttonClear = document.getElementById('clear-board');
-let setPixels = document.getElementsByClassName('pixel');
+const buttonClear = document.getElementById('clear-board');
+const setPixels = document.getElementsByClassName('pixel');
 
 function clearColor() {
-  for(let e of setPixels) {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const e of setPixels) {
     e.style.backgroundColor = 'white';
   }
 }
